29e2f774d8f0d175d0875d27c237a1dc8de5c58a
kawshan <kawshan.virantha9999@gmail.com>
2024-12-02 21:59:02 +0530

    login page

---
+++ app/login/page.jsx
@@ -0,0 +1,9 @@
+"use client"
+export default function LoginPage(){
+    return(
+        <div className="flex flex-col justify-center items-center w-full min-h-screen bg-white text-white text-center">
+            <div className="bg-green-800 px-5 py-3 rounded-lg">login</div>
+        </div>
+
+    )
+}
