def5d7dfc18d9d1482b3bcb4043a606b41e13771
kawshan <kawshan.virantha9999@gmail.com>
2025-01-08 18:52:20 +0530

    21/12/2024 session day 12 class kmp-ts 02.02

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -35,16 +35,17 @@ export default async function DashboardPage() {
                 <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-4">
                     {moviesQuery?.length &&
                         moviesQuery.map((movie) => (
-                            <div key={movie._id} className="h-96">
+                            <div key={movie._id} className="h-[480px]">
                             <Card className="h-full">
                                 <CardHeader>
                                 <CardTitle>{movie?.title}</CardTitle>
                                 <CardDescription className="sr-only">{movie?.title}</CardDescription>
                                 </CardHeader>
                                 <CardContent>
-                                    <div className="flex justify-center bg-black w-full h-[276px] mb-4 rounded">
-                                    <Image src={movie?.poster} alt={movie?.title} width={200} height={400} className="h-full w-auto object-contain"/>
+                                    <div className="flex justify-center bg-black w-full h-[220px] mb-4 rounded">
+                                    <Image src={movie?.poster} alt={movie?.title} width={200} height={400} className="h-full w-auto object-contain" priority={true}/>
                                     </div>
+                                    <p className="line-clamp-3">{movie?.plot}</p>
                                 </CardContent>
                                 <CardFooter></CardFooter>
 
