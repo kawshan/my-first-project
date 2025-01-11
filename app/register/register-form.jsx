00e174361da479feb8abccd248b74de0116618ca
kawshan <kawshan.virantha9999@gmail.com>
2025-01-11 19:38:40 +0530

    vid 12 finished

--- app/register/register-form.jsx
+++ app/register/register-form.jsx
@@ -1,21 +1,56 @@
 "use client"
-import React from 'react';
+import {useState} from 'react';
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
 import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import Link from "next/link";
 
+
+const DEFAULT_ERROR = {
+    error:false,
+    message:"",
+}
+
+
 // keep this as a client component (functional component)
 function RegisterForm() {
+
+    const [error,setError] = useState(DEFAULT_ERROR);
+
+
+    const handleSubmitForm = async (event) => {
+        event?.preventDefault();
+        const formData = new FormData(event?.currentTarget);
+        const name = formData.get("name") ?? "";
+        const email = formData.get("email") ?? "";
+        const password = formData.get("password") ?? "";
+        const confirmPassword = formData.get("confirm-password") ?? "";
+
+
+        console.log("form submitted",{name,email,password,confirmPassword});
+
+        if (name&&email&&password&&confirmPassword){
+            if (password==confirmPassword){
+                setError(DEFAULT_ERROR)
+            }else {
+                setError({error: true,message: "password dosent match"})
+                console.log("error",error);
+            }
+        }
+
+
+    }
+
+
     return (
         <div className="flex justify-center items-center min-h-screen">
-            <Card  className="bg-blue-50/90 w-[350px] ">
+            <Card className="bg-blue-50/90 w-[350px] ">
                 <CardHeader>
                     <CardTitle>Create an Account</CardTitle>
                     <CardDescription>Enter Your Information To Get Started</CardDescription>
                 </CardHeader>
-                <form action="">
+                <form onSubmit={handleSubmitForm}>
                     <CardContent>
                         <div className="flex flex-col space-y-4">
 
@@ -27,20 +62,28 @@ function RegisterForm() {
 
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="email">Email</Label>
-                                <Input id="email" name="email" placeholder="John@example.com"></Input>
+                                <Input id="email" type="email" name="email" placeholder="John@example.com"></Input>
                             </div>
 
 
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="password">Password</Label>
-                                <Input id="password" name="password" placeholder="enter your password"></Input>
+                                <Input id="password" type="password" name="password" placeholder="enter your password"></Input>
                             </div>
 
 
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="confirm-password">Confirm Password</Label>
-                                <Input id="confirm-password" name="confirm-password" placeholder="enter password again to comfirm"></Input>
+                                <Input id="confirm-password" type="password" name="confirm-password"
+                                       placeholder="enter password again to comfirm"></Input>
                             </div>
+
+                            {/*form errors*/}
+                            <div className="flex justify-center">
+                                {error?.error && <span className="text-red-600 text-xs text-center">{error.message}</span>}
+                            </div>
+
+
 
                             <div className="flex justify-center gap-1 text-xs">
                                 Already have an account ?
