ad7dda8e30c3c6af9c3f1d6c14835ebc09be9e2a
kawshan <kawshan.virantha9999@gmail.com>
2025-01-29 21:29:40 +0530

    done untill sign out with next js.. timestamp 00.13.24

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,10 +1,27 @@
+import UserNav from "@/app/dashboard/components/user-nav";
+
 export default function DashBoardLayout({children}) {
     return (
         <div className="flex h-screen overflow-hidden bg-gray-100">
+
+            {/*side panel start*/}
             <aside className="w-64 overflow-y-auto border-r bg-white shadow-lg">Side panel</aside>
-            <div className="bg-gray-500 flex flex-1 flex-col overflow-hidden">
-                <header className="bg-white h-16 items-center gap-4 border-b px-6 shadow-sm">header</header>
+            {/*side panel end*/}
+
+
+            <div className=" flex flex-1 flex-col overflow-hidden">
+
+                {/*dashboard header start*/}
+                <header className="bg-white flex h-16 items-center justify-between gap-4 border-b px-6 shadow-sm">
+                    <h1 className="text-2xl font-bold text-blue-800">Mflix Dashboard</h1>
+                    <UserNav/>
+                </header>
+                {/*dashboard header end*/}
+
+
+                {/*dashboard page body start*/}
                 <main className="flex-1 overflow-y-auto bg-gray-100 p-6">{children}</main>
+                {/*dashboard page body end*/}
 
 
             </div>
